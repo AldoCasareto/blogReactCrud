e0594eb90c3948744e44db9d388447cd684a540f
Aldo <71019940+AldoCasareto@users.noreply.github.com>
2021-06-03 12:05:49 +0200

    forth commit

--- api/models/Post.js
+++ api/models/Post.js
@@ -27,4 +27,4 @@ const PostSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.export = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model('Post', PostSchema);
--- api/routes/users.js
+++ api/routes/users.js
@@ -28,12 +28,12 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-// delete
+// delete user
 
 router.delete('/:id', async (req, res) => {
   if (req.body.userId === req.params.id) {
     try {
-      const user = await User.findByIdAndDelete(req.params.id);
+      const user = await User.findById(req.params.id);
       try {
         await Post.deleteMany({ username: user.username });
         await User.findByIdAndDelete(req.params.id);
@@ -41,9 +41,23 @@ router.delete('/:id', async (req, res) => {
       } catch (error) {
         res.status(500).json(error);
       }
-    } catch (error) {}
+    } catch (error) {
+      res.status(404).json('user not found');
+    }
   } else {
     res.status(401).json('you can only delete your account');
+  }
+});
+
+// get user
+
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
